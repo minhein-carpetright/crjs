63ceb8fd429cf008acd650066d9a7d25c39f8db0
Min <68005418+minhein-carpetright@users.noreply.github.com>
2022-04-06 15:19:41 +0100

    Add files via upload

---
+++ custom2022.js
@@ -0,0 +1,28 @@
+
+var body = document.getElementsByTagName('body')[0];
+if (window.location.pathname == "/") {
+     body.classList.add('homepage');
+     body.classList.remove('internalpages');
+} else {
+     body.classList.add('internalpages');
+     body.classList.remove('homepage');
+}
+
+var tabsBottomWrapper = document.querySelector("#tabsBottomWrapper ._2y6x93");
+var minicartContent = document.querySelector("#minicartContent + ._1x9W3_ h2.GX53aF");
+
+if (tabsBottomWrapper) {
+ tabsBottomWrapper.innerHTML = "<b>Interest free credit</b> available on orders over £1,000";
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+ tabsBottomWrapper.innerHTML = "<b>Interest free credit</b> available on orders over £1,000";
+});
+}
+
+if (minicartContent) {
+ minicartContent.innerHTML = "<b>Interest free credit</b> available on orders over £1,000";
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+ minicartContent.innerHTML = "<b>Interest free credit</b> available on orders over £1,000";
+});
+}
